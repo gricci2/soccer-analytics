8f93086bb8cd4c9bea44dd32029fe809fc49617f
Greggory <greggricci@yahoo.com>
2026-03-13 21:08:30 -0400

    add events dataframe and connect to sql server

--- scripts/events.py
+++ scripts/events.py
@@ -1,5 +1,17 @@
 import json
 import pandas as pd
+import pyodbc
+
+conn = pyodbc.connect(
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=(localdb)\\MSSQLLocalDB;"
+    "DATABASE=SoccerAnalytics;"
+    "Trusted_Connection=yes;"
+)
+
+cursor = conn.cursor()
+
+
 
 file_path = "../data/raw/events/15946.json"
 
@@ -10,9 +22,42 @@ print(f"Total events loaded:  {len(data)}")
 
 df = pd.json_normalize(data)
 
-# all_columns = list(df.columns)
-# print(df.columns.to_list())
-players = df["player.name"]
-unique_players = set(players)
-for player in unique_players:
-    print(player)
+columns = [
+    "id",
+    "index",
+    "period",
+    "timestamp",
+    "minute",
+    "second",
+    "possession",
+    "team.name",
+    "player.name",
+    "type.name",
+    "location",
+    "pass.recipient.name",
+    "pass.length",
+    "shot.statsbomb_xg"
+]
+
+events = df[columns].copy()
+
+events["x"] = events["location"].apply(
+    lambda loc: loc[0] if isinstance(loc, list) else None
+    )
+
+events["y"] = events["location"].apply(
+    lambda loc: loc[1] if isinstance(loc, list) else None
+    )
+events
+
+events = events.drop(columns=["location"])
+events.columns = events.columns.str.replace(".", "_", regex=False)
+print(events.head())
+print(events.info())
+print(events.columns.to_list())
+
+
+#cursor.execute("SELECT DB_NAME()")
+#print(cursor.fetchone())
+#for col in df.columns:
+#    print(col)
