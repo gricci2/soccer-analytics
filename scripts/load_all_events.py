fdf6ba1309d4bf28cb7db5b5baeda1a86769d72a
gricci2 <greggricci@gmail.com>
2026-03-24 21:17:44 -0400

    added load_all_events and load_single_event

---
+++ scripts/load_all_events.py
@@ -0,0 +1,50 @@
+import os
+import json
+import pandas as pd
+import pyodbc
+import numpy as np
+import math
+from load_single_event import load_event
+
+conn = pyodbc.connect(
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=(localdb)\\MSSQLLocalDB;"
+    "DATABASE=SoccerAnalytics;"
+    "Trusted_Connection=yes;"
+)
+
+cursor = conn.cursor()
+cursor.fast_executemany = True
+TABLE = "Events"
+data_dir = "../data/raw/events/"
+
+insert_query = f"""
+        INSERT INTO {TABLE} (
+            id, event_index, period, timestamp, minute, second, possession, 
+            team_name, player_name, type_name,
+            pass_recipient_name, pass_length, shot_statsbomb_xg, x, y
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """
+
+batch_size = 1000
+all_rows = []
+
+for match_file in os.listdir(data_dir):
+    if not match_file.endswith(".json"):
+        continue  # skip non-json files
+    
+    rows = load_event(match_file)
+
+    all_rows.extend(rows)
+
+    if len(all_rows) >= batch_size:
+        cursor.executemany(insert_query, all_rows)
+        all_rows = []
+    
+    #match_path = os.path.join(data_dir, match_file)
+
+if all_rows:
+    cursor.executemany(insert_query, all_rows)
+
+conn.commit()
---
+++ scripts/load_single_event.py
@@ -0,0 +1,62 @@
+import json
+import pandas as pd
+import pyodbc
+import numpy as np
+
+def load_event(file_path):
+    with open(f"../data/raw/events/{file_path}", encoding="utf-8") as f:
+        data = json.load(f)
+    print(f"Total events loaded:  {len(data)}")
+
+    df = pd.json_normalize(data)
+
+    columns = [
+        "id",
+        "index",
+        "period",
+        "timestamp",
+        "minute",
+        "second",
+        "possession",
+        "team.name",
+        "player.name",
+        "type.name",
+        "location",
+        "pass.recipient.name",
+        "pass.length",
+        "shot.statsbomb_xg"
+    ]
+
+    events = df[columns].copy()
+
+    events["x"] = events["location"].apply(
+        lambda loc: loc[0] if isinstance(loc, list) else None
+        )
+
+    events["y"] = events["location"].apply(
+        lambda loc: loc[1] if isinstance(loc, list) else None
+        )
+
+    events = events.drop(columns=["location"])
+    events.columns = events.columns.str.replace(".", "_", regex=False)
+    events = events.rename(columns={"index": "event_index"})
+
+    float_cols = ['pass_length', 'shot_statsbomb_xg', 'x', 'y']
+
+    for col in float_cols:
+        events[col] = events[col].apply(
+            lambda x: float(x) if isinstance(x, (int, float)) and not np.isnan(x) and not np.isinf(x) else None
+        )
+
+        events[col] = events[col].replace({np.nan: None})
+
+    obj_cols = ['player_name', 'pass_recipient_name']
+
+    for col in obj_cols:
+        events[col] = events[col].apply(lambda x: x.strip() if isinstance(x, str) and x.strip() != "" else None)
+
+        events[col] = events[col].replace({np.nan: None})
+
+    rows = list(events.itertuples(index=False, name=None))
+    print(len(rows[0]))
+    return rows
