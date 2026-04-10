cfeb33f4915418712e78b80c2bec8936897d16b4
gricci2 <greggricci@gmail.com>
2026-04-09 21:37:11 -0400

    cleaned up comments

--- scripts/visualization/pass_locations_all_maps.py
+++ scripts/visualization/pass_locations_all_maps.py
@@ -9,24 +9,8 @@ passes_df = passes_df.dropna(subset=['x', 'y'])
 
 passes_df = passes_df.sample(n=200000, random_state=42)
 
-#team looping removed for heatmap
-#teams = passes_df['team_name'].unique()
-
 pitch = Pitch(pitch_type='statsbomb', pitch_color='grass', line_color='white')
 fig, ax = pitch.draw(figsize=(12, 8))
-
-# for team in teams:
-#     subset = passes_df[passes_df['team_name']== team]
-#     pitch.scatter(subset['x'], subset['y'], ax=ax, alpha=0.5, label=team, s=100)
-
-# bin_stat = pitch.bin_statistic(
-#     passes_df['x'],
-#     passes_df['y'],
-#     statistic='count',
-#     bins=(25, 25)
-# )
-
-# pitch.heatmap(bin_stat, ax=ax, cmap='Reds')
 
 sns.kdeplot(
     x=passes_df['x'],
@@ -38,9 +22,5 @@ sns.kdeplot(
     ax=ax
 )
 
-
-
-#plt.title("Pass Starting Locations by Team (mplsoccer)")
-#plt.legend()
 plt.title("Pass Heatmap (All Teams)")
 plt.show()
